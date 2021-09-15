74c70c860c350a149bbda3ba97e831bb6f260adc
andrei <git1@email.com>
2021-09-15 17:08:16 +0200

    updated App
    updated App.css styles and added passing Video props in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-// Styles
-import "./App.css";
 // Components
 import Video from "./components/Video/Video";
+// Video
+import { HAPPY_DOG_VIDEO } from "./config";
+// Styles
+import "./App.css";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <div className="app__videos">
-        <Video />
+        <Video
+          url={HAPPY_DOG_VIDEO}
+          song="🎵James Brown - I feel good!😃😄🤪🥳"
+        />
         <Video />
         <Video />
       </div>

d243f5fcf1a355a820cc632178abef6d664ff5de
andrei <git1@email.com>
2021-09-15 17:04:02 +0200

    added Video implementation
    added Play/Pause, props

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,22 +1,39 @@
-import React from "react";
-// Video
-import { HAPPY_DOG_VIDEO } from "../../config";
+import React, { useRef, useState } from "react";
 // Components
 import VideoFooter from "../VideoFooter/VideoFooter";
 import VideoSidebar from "../VideoSidebar/VideoSidebar";
 // Styles
 import "./Video.css";
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
+  const [play, setPlay] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  };
+
+  // TODO: Hide player controls
+
   return (
     <div className="video">
       <iframe
         title="happy dog"
         className="video__player"
-        src={HAPPY_DOG_VIDEO}
+        src={url}
+        onClick={onVideoPress}
+        ref={videoRef}
+        allow="fullscreen"
+        frameBorder="0"
       />
-      <VideoFooter />
-      <VideoSidebar />
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} messages={messages} shares={shares} />
     </div>
   );
 }

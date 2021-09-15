963abe19faf086efd1c31955e01b1b5d0afe6b29
andrei <git1@email.com>
2021-09-15 17:05:30 +0200

    added VideoSidebar
    added VideoFooter component implementation + css styles

--- src/components/VideoSidebar/VideoSidebar.js
+++ src/components/VideoSidebar/VideoSidebar.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 // Styles
-import './VideoSidebar.css'
+import "./VideoSidebar.css";
+// Icons
+import FavouriteIcon from "@material-ui/icons/Favorite";
+import FavouriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
 
-function VideoSidebar() {
-    return (
-        <div>
-            
-        </div>
-    )
+function VideoSidebar({ likes, messages, shares }) {
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <div className="videoSidebar">
+      <div className="videoSidebar__button">
+        {liked ? (
+          <FavouriteIcon fontSize="large" onClick={(e) => setLiked(false)} />
+        ) : (
+          <FavouriteBorderIcon
+            fontSize="large"
+            onClick={(e) => setLiked(true)}
+          />
+        )}
+        <p>{liked ? likes + 1 : likes}</p>
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon fontSize="large" />
+        <p>{messages}</p>
+      </div>
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize="large" />
+        <p>{shares}</p>
+      </div>
+    </div>
+  );
 }
 
-export default VideoSidebar
+export default VideoSidebar;

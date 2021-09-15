5efc08c4ee79af311d75fc6894e45ddc75c5ae28
andrei <git1@email.com>
2021-09-15 17:05:02 +0200

    added VideoFooter
    added VideoFooter component implementation + css styles

--- src/components/VideoFooter/VideoFooter.js
+++ src/components/VideoFooter/VideoFooter.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
 // Styles
-import './VideoFooter.css'
+import "./VideoFooter.css";
+// Icons
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+// Ticker
+import Ticker from "react-ticker";
 
-function VideoFooter() {
-    return (
-        <div>
-            
+function VideoFooter({ channel, descripion, song }) {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@{channel}</h3>
+        <p>{descripion}</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+          <Ticker mode="smooth">
+            {({ idex }) => (
+              <>
+                <p>{song}</p>
+              </>
+            )}
+          </Ticker>
         </div>
-    )
+      </div>
+      <img
+        src="https://static.thenounproject.com/png/934821-200.png"
+        alt=""
+        className="videoFooter__record"
+      />
+    </div>
+  );
 }
 
-export default VideoFooter
+export default VideoFooter;
